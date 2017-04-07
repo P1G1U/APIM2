3444fc3abc5150a4a4958a594a46ab3d51992988
Pietro <pietrolonardi@gmail.com>
2017-04-07 10:17:09 +0000

    Upload new file

---
+++ progetto/backend/ApiM/src/com/apim/server/entities/Categoria.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Pigiu on 06/04/2017.
+ */
+public class Categoria {
+    String nome;
+
+    Categoria(){
+        nome="";
+    }
+
+    Categoria(String n){
+        nome=n;
+    }
+
+    Categoria(Categoria c){
+        nome= c.nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
