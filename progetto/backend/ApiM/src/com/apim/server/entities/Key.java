97a7332a84f157927bd2157848ccaa2441ecba66
Pietro <pietrolonardi@gmail.com>
2017-04-07 10:17:43 +0000

    Upload new file

---
+++ progetto/backend/ApiM/src/com/apim/server/entities/Key.java
@@ -0,0 +1,63 @@
+import java.util.Date;
+
+/**
+ * Created by Pigiu on 06/04/2017.
+ */
+public class Key {
+    String id;
+    Date dataScadenza;
+    double maxByte, tempoUso;
+
+    Key(){
+        id="";
+        dataScadenza=null;
+        maxByte=0;
+        tempoUso=0;
+    }
+
+    Key(String i, Date d, double m, double t){
+        id=i;
+        dataScadenza=d;
+        maxByte=m;
+        tempoUso=t;
+    }
+
+    Key(Key k){
+        id= k.id;
+        dataScadenza=k.dataScadenza;
+        maxByte=k.maxByte;
+        tempoUso=k.tempoUso;
+    }
+
+    public Date getDataScadenza() {
+        return dataScadenza;
+    }
+
+    public double getMaxByte() {
+        return maxByte;
+    }
+
+    public double getTempoUso() {
+        return tempoUso;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setDataScadenza(Date dataScadenza) {
+        this.dataScadenza = dataScadenza;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setMaxByte(double maxByte) {
+        this.maxByte = maxByte;
+    }
+
+    public void setTempoUso(double tempoUso) {
+        this.tempoUso = tempoUso;
+    }
+}
