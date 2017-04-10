426bcbb7ff920508862dacd311c9c292e079c233
Pietro <pietrolonardi@gmail.com>
2017-04-10 10:45:37 +0000

    Upload new file

---
+++ progetto/backend/ApiM/src/com/apim/server/services/ServicePutUser.java
@@ -0,0 +1,15 @@
+package com.apim.server.services;
+
+import com.apim.server.entities.User;
+import com.apim.server.dao;
+
+/**
+ * Created by Pigiu on 10/04/2017.
+ */
+public class ServicePutUser {
+    public boolean putUser(User u){
+        //il metodo inserisce un nuovo user nel DB, se l'operazione va a buon fine viene ritornato un valore true, altrimenti fals
+
+        return DAOUser.putUser(u);
+    }
+}
