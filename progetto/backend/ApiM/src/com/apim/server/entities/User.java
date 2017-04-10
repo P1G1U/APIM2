00058854acbabf37f887d2c6831c0871b407bad3
Pietro <pietrolonardi@gmail.com>
2017-04-10 09:53:31 +0000

    Update User.java

--- progetto/backend/ApiM/src/com/apim/server/entities/User.java
+++ progetto/backend/ApiM/src/com/apim/server/entities/User.java
@@ -1,7 +1,9 @@
+package com.apim.server.entities;
+
 import java.util.Date;
 
 /**
- * Created by Pigiu on 06/04/2017.
+ * Created on 06/04/2017.
  */
 public class User {
     String username, password, nome, cognome, mail, numeroCarta, indirizzo, telefono, fotoProfilo, bio;
