e439b59dcc33e8e4c664616d178fe2721719ad5b
Pietro <pietrolonardi@gmail.com>
2017-04-10 10:44:55 +0000

    Upload new file

---
+++ progetto/backend/ApiM/src/com/apim/server/services/ServiceUserManager.java
@@ -0,0 +1,31 @@
+package com.apim.server.services;
+
+import  jolie.runtime.JavaService;
+import com.apim.server.entities.User;
+
+/**
+ * Created by Pigiu on 04/04/2017.
+ */
+
+public class ServiceUserManager extends JavaService {
+    private ServiceGetUser gU;
+    private ServicePutUser pU;
+    private ServiceUpdateUser uU;
+    private ServiceDeleteUser dU;
+
+    public User getUser(String id){
+        return gU.getUser(id);
+    }
+
+    public boolean putUser(User u){
+        return pU.putUser(u);
+    }
+
+    public User updateUser(User u){
+        return uU.updateUser(u);
+    }
+
+    public boolean deleteUser(String id){
+        return dU.deleteUser(id);
+    }
+}
