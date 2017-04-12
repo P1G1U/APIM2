15a6b1238033c3f0b3f562be3c06184b14a2dc08
Sebastiano Marchesini <sebastiano.marchesini@gmail.com>
2017-04-12 12:17:40 +0200

    Modificate le assegnazioni Jumbtron

--- progetto/APIM/src/app/jumbotron.component.ts
+++ progetto/APIM/src/app/jumbotron.component.ts
@@ -3,18 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector : 'jumbotron',
   templateUrl : 'jumbotron.component.html'
+
 })
 
 export class JumbotronComponent {
-  public jbtHeading:string;
-  public jbtText:string;
-  public jbtBtnText:string;
-  public jbtBtnUrl:string;
-
-  costructor(){
-      this.jbtHeading = ' Welcome to API Market of Microservices ';
-      this.jbtText = "qua ci scriviamo qualcosa di descrizione del progetto";
-      this.jbtBtnText = "Read More";
-      this.jbtBtnUrl = "http://jolie-lang.org";
-  }
+  jbtHeading = "Welcome to API Market of Microservices";
+  jbtText = "qua ci scriviamo qualcosa di descrizione del progetto";
+  jbtBtnText = "Read More";
+  jbtBtnUrl = "http://jolie-lang.org";
 }
