1818cafd4685dcdec4dcbddafd59a53d6fa26715
Piergiorgio <piergiorgio.danieli@gmail.com>
2017-04-13 09:43:34 +0000

    DAO

---
+++ progetto/backend/ApiM/src/com/apim/server/DAO/DAOUser.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Utente on 12/04/2017.
+ */
+import com.apim.server.dao;
+import javax.persistence.EntityManager;
+public class DAOUser implements IDAOUser {
+    private EntityManager em;
+    public void remove(User user){
+        em.remove(User);
+    }
+    public User load(string username){
+        return em.find(User.class username);
+    }
+    public User save(User user){
+        em.persist(User);
+        return User;
+    }
+    public User update (User user){
+        return em.merge(User);
+    }
+    public void setEm(EntityManager em){
+        this.em = em;
+    }
+    public EntityManager getEm(){
+        return em;
+    }
+}
